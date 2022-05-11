95409ffe66a1dbafd3d26959675fd0c0b94cc6e8
Chris <cjmugs@gmail.com>
2022-05-11 10:50:32 -0500

    File updates

--- garage_door.py
+++ garage_door.py
@@ -1,8 +1,13 @@
 from tkinter import *
 from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+
+
+def new_gui():
+    root = Tk()
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="Basic GUI").grid(column=0, row=0)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    root.mainloop()
+    
+new_gui()
